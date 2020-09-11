481399edee115fa450acd7bc381052f890433249
SparTT <leoguedes3011@gmail.com>
2020-09-11 13:08:16 -0300

    Updates

---
+++ precache-manifest.6deaca1909dad92a289fad4d503f638d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9c4df9f10010ea8c9d9171ccd52336",
+    "url": "/crypto/index.html"
+  },
+  {
+    "revision": "5e594753eaa57c29b34b",
+    "url": "/crypto/static/css/main.af376ccd.chunk.css"
+  },
+  {
+    "revision": "a935aaa9bc46128e3f42",
+    "url": "/crypto/static/js/2.459bb840.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto/static/js/2.459bb840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e594753eaa57c29b34b",
+    "url": "/crypto/static/js/main.e7316162.chunk.js"
+  },
+  {
+    "revision": "5a8a76bd7145105229c5",
+    "url": "/crypto/static/js/runtime-main.b477169b.js"
+  }
+]);
